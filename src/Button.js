ed83d896843ed048a9a36c2fb222181fefd34973
Cilla <57146695+Pensum84@users.noreply.github.com>
2021-01-18 14:00:20 +0100

    uppdaterig

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import'./Button.css';
+import './Button.css';
 
 const STYLES = ['btn--primary','btn--outline']
 const Sizes = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide']
 const COLOR = ['primary', 'blue', 'red', 'green']
 
 export const Button = ({children, type, onclick, buttonStyle, buttonSize, buttonColor}) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle)
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? 
+    buttonStyle : STYLES[0]
+
+    const checkButtonSize = STYLES.includes(buttonSize) ?
+        buttonSize : SIZES[0]    
+
+    const checkButtonColor = COLOR.includes(buttonColor) ?
+        buttonColor : COLOR[0]  
 
     return(
         <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
